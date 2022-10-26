52947fcdfcf55cf78952a7fd6d59be6d08d2d8a0
aaronminaya0 <aaronminaya0@gmail.com>
2022-10-26 14:39:20 -0400

    created sidebar

---
+++ project_page/project.js
@@ -0,0 +1,20 @@
+// Document variables
+const mySidebar = document.getElementById("mySidebar")
+const main =  document.getElementById("main")
+const closeSideBar = document.querySelector('.closebtn')
+const openSideBar = document.querySelector('.openbtn')
+
+// Event Listeners
+closeSideBar.addEventListener("click", closeNav)
+openSideBar.addEventListener("click", openNav)
+
+// Functions
+function closeNav() {
+    mySidebar.style.width = "0px";
+    main.style.marginLeft = "0px";
+}
+
+function openNav() {
+    mySidebar.style.width = "250px";
+    main.style.marginLeft = "250px";
+}
