331f8048406ab749600fa6ec976eaf07736e7425
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2022-10-26 14:28:38 -0400

    added name columnd to projects table

--- migrations/20221026144608_create_projects_table.js
+++ migrations/20221026144608_create_projects_table.js
@@ -5,6 +5,7 @@
 exports.up = function (knex) {
     return knex.schema.createTable('projects', table => {
         table.increments('id').primary();
+        table.string('name').notNullable;
         table.string('description').notNullable;
     })
 };
--- seeds/02_seed.js
+++ seeds/02_seed.js
@@ -6,6 +6,6 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('projects').del()
   await knex('projects').insert([
-    { id: 1, description: 'Social Media Application' }
+    { id: 1, name: 'Meta', description: 'Social Media Application' }
   ]);
 };
