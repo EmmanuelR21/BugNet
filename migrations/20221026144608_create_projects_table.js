52f5820ce1faff7fda47f0962810a54217b2f559
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2022-10-26 11:03:46 -0400

    created migration files

---
+++ migrations/20221026144608_create_projects_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    knex.schema.createTable('projects', table => {
+        table.increments('id').primary();
+        table.string('description').notNullable;
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    knex.schema.dropTable('projects');
+};
---
+++ migrations/20221026144953_create_issues_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    knex.schema.createTable('issues', table => {
+        table.increments('id').primary();
+        table.string('description').notNullable;
+        table.string('code').notNullable;
+        table.string('status').notNullable;
+        table.string('code_feedback');
+        table.string('comment_feedback');
+        table.string('project_id').notNullable;
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    knex.schema.dropTable('issues')
+};
