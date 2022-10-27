b90276e4dec612cf567a690b7eaf8c17146134d1
aaronminaya0 <aaronminaya0@gmail.com>
2022-10-27 17:02:44 -0400

    created test DB through seeds

--- seeds/02_seed.js
+++ seeds/02_seed.js
@@ -6,6 +6,11 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('projects').del()
   await knex('projects').insert([
-    { id: 1, name: 'Meta', description: 'Social Media Application' }
+    { id: 1, name: 'Meta', description: 'Social Media Application'},
+    { id: 2, name: 'UMVC3', description: 'Fighting game'},
+    { id: 3, name: 'Scarab', description: 'Archeoloy project'},
+    { id: 4, name: 'spaceX', description: 'interloper'},
+    { id: 5, name: 'butterfly', description: 'evolution project'},
+    { id: 6, name: 'python', description: 'private python community'},
   ]);
 };
