bed60ba57de814ba46fda42a235eb2140a3424ca
aaronminaya0 <aaronminaya0@gmail.com>
2022-10-26 21:42:58 -0400

    changed controller parameters to req, res

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const projectsController = require('./controllers/projectsController.js')
 const app = express();
 const PORT = process.env.PORT || 5432; // Or whichever port you choose for your local server
+
+app.get('/project/:user_id', projectsController.grabAllProjects)
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,12 +1,16 @@
 const Project = require('../models/projectsModel.js');
 
-const grabAllProjects = async (userId) => {
-    const projects = await Project.grabAllProjectsOfUserFromDb(userId);
+const grabAllProjects = async (request, response) => {
+    const userid = request.params.user_id
+    const projects = await Project.grabAllProjectsOfUserFromDb(userid);
     console.log(projects.rows);
 }
 
-const grabProjectIssues = async (name) => {
-    const ID = await Project.grabIdOfProjectFromDb(name)
+const grabProjectIssues = async (requset, response) => {
+    const projectName = requset.params.name
+    const ID = await Project.grabIdOfProjectFromDb(projectName)
     const issues = await Project.grabAllIssuesFromProjectFromDb(ID)
     console.log(issues.rows)
 }
+
+module.exports = {grabAllProjects, grabProjectIssues}
