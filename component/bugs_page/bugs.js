ca31fff48a795a8f35d838b4acfe8cabf20a7c75
AGarciaNY <aldairgarcia360@gmail.com>
2022-10-30 13:56:42 -0400

    feat: formated project js

--- component/bugs_page/bugs.js
+++ component/bugs_page/bugs.js
@@ -1,3 +1,19 @@
+let logedinusername = localStorage.getItem("logedinusername");
+let logedinpassword = localStorage.getItem("logedinpassword");
+let logedinId = localStorage.getItem("userid")
+let logedin = localStorage.getItem("logedin");
+let currentProject = localStorage.getItem("currentProjectid");
+if (logedin === "false") {
+    location.replace("../../index.html")
+}
+console.log(logedinusername, logedinpassword, logedinId, logedin, currentProject)
+let logOutButton = document.getElementById("log-out-button")
+logOutButton.addEventListener("click", () => {
+    localStorage.setItem("logedinusername", null);
+    localStorage.setItem("logedinpassword", null);
+    localStorage.setItem("logedin", false);
+    location.replace("../../index.html")
+})
 // Document variables
 const querrybtn = document.querySelector('.querrybtn')
 const bugsDiv = document.querySelector('.bugs')
