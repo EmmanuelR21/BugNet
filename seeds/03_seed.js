e68e4b408ca5ad0eb16a9216353cbc338ac7f226
aaronminaya0 <aaronminaya0@gmail.com>
2022-10-30 20:41:12 -0400

    bug title and description now appear

--- seeds/03_seed.js
+++ seeds/03_seed.js
@@ -9,6 +9,7 @@ exports.seed = async function (knex) {
     { bug_id: 1, project_id: 1, user_id: 1, title: 'friends tab not working', description: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah', code: 'friends.push(x)', status: 'todo', feedback: null, code_feedback: null, },
     { bug_id: 2, project_id: 1, user_id: 1, title: 'my dog ate my homework', description: 'rah rah rah rah rah rah rah rah rah rah rah rah rah rah rah rah rah', code: 'roscoStomach.push(homework)', status: 'review', feedback: null, code_feedback: null, },
     { bug_id: 3, project_id: 1, user_id: 1, title: 'a bug flew in my nose', description: 'oga-boga oga-boga oga-boga oga-boga oga-boga oga-boga oga-boga oga-boga', code: 'myNose.push(bug)', status: 'completed', feedback: null, code_feedback: null, },
-    { bug_id: 4, project_id: 1, user_id: 1, title: 'trying to console log 0-10', description: 'waka-flaka waka-flaka waka-flaka waka-flaka waka-flaka waka-flaka waka-flaka ', code: 'for (let i = 1; i < 10; i++) {console.log(i)}', status: 'tdo', feedback: null, code_feedback: null, },
+    { bug_id: 4, project_id: 1, user_id: 1, title: 'trying to console log 0-10', description: 'waka-flaka waka-flaka waka-flaka waka-flaka waka-flaka waka-flaka waka-flaka ', code: 'for (let i = 1; i < 10; i++) {console.log(i)}', status: 'todo', feedback: null, code_feedback: null, },
+    { bug_id: 5, project_id: 1, user_id: 1, title: 'im so tired', description: 'whoops', code: 'something.something = anything', status: 'completed', feedback: null, code_feedback: null, },
   ]);
 };
