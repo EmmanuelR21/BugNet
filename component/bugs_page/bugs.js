065efaf5535593d12ffa9c1bbe94440f0d7cd102
aaronminaya0 <aaronminaya0@gmail.com>
2022-10-31 00:02:05 -0400

    saving

--- component/bugs_page/bugs.js
+++ component/bugs_page/bugs.js
@@ -13,6 +13,11 @@ let closeIndividualTask = document.getElementById("close-individual-task-form")
 const formText = document.querySelector('#form-text')
 const formDescription = document.querySelector('#form-description')
 
+const postBugTitle = document.querySelector('#new-task-title')
+const postBugDesc = document.querySelector('#new-task-form')
+const postBugCode = document.querySelector('#new-task-code')
+const postBugBtn = document.querySelector('#new-task-button')
+
 if (logedin === "false") {
     location.replace("../../index.html")
 }
@@ -42,9 +47,38 @@ closeIndividualTask.addEventListener("click", () => {
     newTaskFormBackground.style.display = "none"
 })
 
+postBugBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log('hi')
+
+    var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "user_id": localStorage.userid,
+  "projectId": "1",
+  "title": postBugTitle.value,
+  "description": postBugDesc.value,
+  "code": postBugCode.value
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("http://localhost:5432/bugs/", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+})
+
 // Functions
 async function pullBugs() {
-    const response = await fetch(`https://localhost:5432/bugs/${localStorage.getItem("currentProjectid")}`)
+    const response = await fetch(`http://localhost:5432/bugs/${localStorage.getItem("currentProjectid")}`)
     const json = await response.json()
     for (const bugs of json) {
         const div = document.createElement('div')
